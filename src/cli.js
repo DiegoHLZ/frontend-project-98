b0ebe5764f85840254055136ede3680a5a403823
unknown <huinchodiego7@icloud.com>
2024-08-05 22:09:44 -0500

    Agregar brain-even.js y modifique el package.json

--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,8 @@
-// src/cli.js
 import readlineSync from 'readline-sync';
 
-const greetUser = () => {
-  console.log('¡Bienvenido a Brain Games!');
+const getUserName = () => {
   const name = readlineSync.question('¿Cual es tu nombre? ');
-  console.log(`¡Hola, ${name}!`);
+  return name;
 };
 
-export default greetUser;
+export default getUserName;
