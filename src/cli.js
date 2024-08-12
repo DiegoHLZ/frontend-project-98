1c0402b94af547a2fbc001fa25e3d433a48dbf21
unknown <huinchodiego7@icloud.com>
2024-08-12 18:39:55 -0500

    Add greeting message to cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
 
 const getUserName = () => {
+  console.log('¡Bienvenido a Brain Games!');
   const name = readlineSync.question('¿Cual es tu nombre? ');
   return name;
 };
