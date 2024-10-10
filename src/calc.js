e58d88d2a2e7bf7848b114391f387f458902a964
diego <u201923466@upc.edu.pe>
2024-10-10 11:43:55 -0500

    arreglar el codigo del archivo calc

--- src/calc.js
+++ src/calc.js
@@ -1,18 +1,24 @@
 import { getRandomNumber, getUserAnswer, checkAnswer } from './utils.js';
-import greetUser from './cli.js'; // obtenemos el nombre del usuario
+import greetUser from './cli.js';
 
 const brainCalcGame = () => {
-  const userName = greetUser();
+  const userName = greetUser(); 
   console.log('Resuelve las operaciones matemáticas.');
 
   const maxRounds = 3;
   for (let round = 0; round < maxRounds; round += 1) {
-    const firstRandomNumber = getRandomNumber(10); //numeros aleatorios
+    const firstRandomNumber = getRandomNumber(10);
     const secondRandomNumber = getRandomNumber(10);
+    
+    // Generamos la operación de suma 
     const correctAnswer = firstRandomNumber + secondRandomNumber;
 
+    console.log(`Pregunta: ${firstRandomNumber} + ${secondRandomNumber}`);
+    
+    // Obtenemos la respuesta del usuario
     const userAnswer = getUserAnswer('¿Cuánto es?', `${firstRandomNumber} + ${secondRandomNumber}`, true);
-
+    
+    // Comparamos la respuesta y mostramos el mensaje adecuado
     if (!checkAnswer(userAnswer, correctAnswer, userName)) {
       return;
     }
