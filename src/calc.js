c72fca37f0ff187b9ed48f97ec104d29b4aedde2
diego <u201923466@upc.edu.pe>
2024-10-10 12:03:29 -0500

    aumento operaciones al archivo calc

--- src/calc.js
+++ src/calc.js
@@ -2,23 +2,40 @@ import { getRandomNumber, getUserAnswer, checkAnswer } from './utils.js';
 import greetUser from './cli.js';
 
 const brainCalcGame = () => {
-  const userName = greetUser(); 
+  const userName = greetUser();
   console.log('Resuelve las operaciones matemáticas.');
 
   const maxRounds = 3;
-  for (let round = 0; round < maxRounds; round += 1) {
-    const firstRandomNumber = getRandomNumber(10);
-    const secondRandomNumber = getRandomNumber(10);
-    
-    // Generamos la operación de suma 
-    const correctAnswer = firstRandomNumber + secondRandomNumber;
+  const operations = ['+', '-', '*']; 
 
-    console.log(`Pregunta: ${firstRandomNumber} + ${secondRandomNumber}`);
+  for (let round = 0; round < maxRounds; round += 1) {
+    const firstRandomNumber = getRandomNumber(10); 
+    const secondRandomNumber = getRandomNumber(10);
+    const randomOperation = operations[Math.floor(Math.random() * operations.length)]; 
     
-    // Obtenemos la respuesta del usuario
-    const userAnswer = getUserAnswer('¿Cuánto es?', `${firstRandomNumber} + ${secondRandomNumber}`, true);
+    // Calculamos la respuesta correcta basada en la operación
+    let correctAnswer;
+    switch (randomOperation) {
+      case '+':
+        correctAnswer = firstRandomNumber + secondRandomNumber;
+        break;
+      case '-':
+        correctAnswer = firstRandomNumber - secondRandomNumber;
+        break;
+      case '*':
+        correctAnswer = firstRandomNumber * secondRandomNumber;
+        break;
+      default:
+        correctAnswer = null;
+    }
+
+    // Mostrar la pregunta
+    console.log(`Pregunta: ${firstRandomNumber} ${randomOperation} ${secondRandomNumber}`);
     
-    // Comparamos la respuesta y mostramos el mensaje adecuado
+    // Obtener la respuesta del usuario
+    const userAnswer = getUserAnswer('¿Cuánto es?', `${firstRandomNumber} ${randomOperation} ${secondRandomNumber}`, true);
+
+    // Verificar si la respuesta es correcta
     if (!checkAnswer(userAnswer, correctAnswer, userName)) {
       return;
     }
